ee7ffecd5c9d38d9f81edc28d30dcf4cebe2144e
farrukh90 <farrukhsadykov@gmail.com>
2022-03-09 22:35:21 +0000

    added host

--- 9.vault-setup/vault.tf
+++ 9.vault-setup/vault.tf
@@ -16,6 +16,9 @@ module "vault-terraform-helm" {
       ingress.kubernetes.io/ssl-redirect: "false"
       cert-manager.io/cluster-issuer: letsencrypt-prod
       acme.cert-manager.io/http01-edit-in-place: "true"
+    ingressClassName: "nginx"
+    hosts:
+      - host: "vault.${var.google_domain_name}"
   EOF
 }
 

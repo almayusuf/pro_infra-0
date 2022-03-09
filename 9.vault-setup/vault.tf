a09aa9864ec14488e767946e8d464f65028c7a76
farrukh90 <farrukhsadykov@gmail.com>
2022-03-09 22:31:28 +0000

    vault

--- 9.vault-setup/vault.tf
+++ 9.vault-setup/vault.tf
@@ -8,6 +8,31 @@ module "vault-terraform-helm" {
   deployment_name      = "vault"
   deployment_namespace = "vault"
   deployment_path      = "charts/vault/"
-  values_yaml          = ""
+  values_yaml          = <<EOF
+  ingress:
+    enabled: true
+    annotations: 
+      nginx.ingress.kubernetes.io/proxy-body-size: "0"
+      ingress.kubernetes.io/ssl-redirect: "false"
+      cert-manager.io/cluster-issuer: letsencrypt-prod
+      acme.cert-manager.io/http01-edit-in-place: "true"
+  ingressClassName: "nginx"
+    hosts:
+    - host: "vault.${var.google_domain_name}"
+        http:
+        paths:
+        - pathType: Prefix
+            path: "/"
+            backend:
+            service:
+                name: vault
+                port:
+                number: 8200
+
+    tls: 
+      - secretName: chart-example-tls
+        hosts:
+          - "vault.${var.google_domain_name}"
+  EOF
 }
 

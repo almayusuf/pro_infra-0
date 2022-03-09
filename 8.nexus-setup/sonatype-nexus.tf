58927cbda0a077b7e75bf10c5a08ba3ea970ce8d
farrukh90 <farrukhsadykov@gmail.com>
2022-03-09 21:51:35 +0000

    added ns

--- 8.nexus-setup/sonatype-nexus.tf
+++ 8.nexus-setup/sonatype-nexus.tf
@@ -1,7 +1,7 @@
-#module "sonatype-nexus-terraform-k8s-namespace" {
-#  source               = "./modules/terraform-k8s-namespace/"
-#  deployment_namespace = "sonatype-nexus"
-#}
+module "sonatype-nexus-terraform-k8s-namespace" {
+  source               = "./modules/terraform-k8s-namespace/"
+  deployment_namespace = "sonatype-nexus"
+}
 
 module "sonatype-nexus-terraform-helm" {
   source               = "./modules/terraform-helm/"

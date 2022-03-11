c04cad972e21c32b11cc28cd24177ce68075bcea
farrukh90 <farrukhsadykov@gmail.com>
2022-03-11 03:17:23 +0000

    added node count

--- 2.cluster_setup/module/main.tf
+++ 2.cluster_setup/module/main.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "primary" {
 	name = var.gke_config["cluster_name"]
 	location = var.gke_config["region"]
 	remove_default_node_pool = false
-	initial_node_count = 1
+	initial_node_count = var.gke_config["node_count"]
 	master_auth {
 		client_certificate_config {
 		issue_client_certificate = false

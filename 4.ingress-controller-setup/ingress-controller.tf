89b304294b7b26f13880e24c99610e10902292ac
farrukh90 <farrukhsadykov@gmail.com>
2022-03-28 21:01:21 +0000

    added ip

--- 4.ingress-controller-setup/ingress-controller.tf
+++ 4.ingress-controller-setup/ingress-controller.tf
@@ -14,13 +14,14 @@ controller:
     create: true
     type: LoadBalancer
     loadBalancerSourceRanges: 
-    - "104.181.144.186/32"   # Evolve Cyber Office
-    - "72.68.108.222/32"     # Maksat Sadyrov
-    - "73.74.242.241/32"     # Azizbek 
-    - "99.7.19.155/32"       # DJ
-    - "104.57.11.55/32"      # Suheda
-    - "73.8.110.80/32"       # Beibars
-    - "147.219.244.131/32"   # Altai
+      "104.181.144.186/32"   # Evolve Cyber Office
+      "72.68.108.222/32"     # Maksat Sadyrov
+      "73.74.242.241/32"     # Azizbek 
+      "99.7.19.155/32"       # DJ
+      "104.57.11.55/32"      # Suheda
+      "73.8.110.80/32"       # Beibars
+      "147.219.244.131/32"   # Altai
+      "104.185.25.250/32",   # Anastasia
   EOF
 }
 
--- 4.ingress-controller-setup/variable.tf
+++ 4.ingress-controller-setup/variable.tf
@@ -1,3 +1,3 @@
 # variable "loadBalancerSourceRanges" {
-#   type = list(any)
+#   type = list
 # }
